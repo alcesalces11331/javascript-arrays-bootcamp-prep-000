1a66a53b7c62b267bb73e74b86009010190cd1e8
Rollin Metzger <rollin.metzger@gmail.com>
2017-12-05 04:49:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,11 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
  array.unshift(element)
   return array 
 }
+
+function addElementToEndOfArray(array, element) {
+  array = [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+}
